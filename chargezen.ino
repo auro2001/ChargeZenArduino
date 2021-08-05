1c867ad0eae32a8fd1aec6d4dd77acce2f52b355
Shre Aurobind Raam T <52354180+auro2001@users.noreply.github.com>
2021-08-05 12:43:16 +0530

    Update chargezen.ino

--- chargezen.ino
+++ chargezen.ino
@@ -8,13 +8,9 @@ int tempPin = 3;
 const int analogPin = A1;
 float analogValue;
 float input_voltage;
-//int delayTime = 100;
 int latchPin = 6;//ST
 int clockPin = 5;//SH
 int dataPin = 7;//DS 
-
-
-/*-----------------------------------------Declarations--------------------------------------------*/
 
 void setup()
 {
@@ -30,8 +26,6 @@ void setup()
   
 }
 
-/*----------------------------------------------------setup-------------------------------------------------*/
-
 void updateShiftRegister(byte storageByte)
 {
   digitalWrite(latchPin, LOW);
@@ -40,8 +34,6 @@ void updateShiftRegister(byte storageByte)
 
   digitalWrite(latchPin, HIGH);
 }
-
-/*-------------------------------------------------74HC595------------------------------------------------*/
 
 void temp()
 {
@@ -56,196 +48,142 @@ void temp()
   Serial.println();
 }
 
-/*-------------------------------------------------Temperature----------------------------------------------*/
+void bluetoot()
+{
+  while(!bluetooth.available());
+    char value = bluetooth.read();
+    switch(value)
+    {
+      case 'O':
+        digitalWrite(relay,LOW);
+        break;
+      case 'F':
+        digitalWrite(relay,HIGH);
+        break;
+    }
+}
 
 void loop()
 {
-   digitalWrite(relay,LOW);
+   digitalWrite(relay,HIGH);
    analogValue = analogRead (A1);
    Serial.println(analogValue);
    delay (1000); 
-   input_voltage = (analogValue*8.7) / 1023.0;
+   input_voltage = (analogValue*9) / 1023.0;
    Serial.println("Voltage= ");
    Serial.println(input_voltage);
    delay(1000);
   
   if (input_voltage < 2.50 && input_voltage >= 0.00 )
   { 
-    digitalWrite(relay,LOW);
-    delay(1000);
-    Serial.println("On");
     byte storageByte = 0x80; 
     updateShiftRegister(storageByte);
-    temp();
-    Serial.println("Percentage: 0");
     digitalWrite(buzz, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(buzz, LOW);
-    //delay (5000);
+    delay (1000);
     digitalWrite(led, HIGH);
     delay (5000);
     digitalWrite(led, LOW);
-    digitalWrite(led, HIGH);
-    //delay (5000);
+    delay (1000);
     Serial.println("Drain");
     Serial.println("Health:Bad");
-    while(!bluetooth.available());
-    char value = bluetooth.read();
-    switch(value)
-    {
-      case 'O':
-        digitalWrite(relay,LOW);
-        Serial.println("On");
-        break;
-      case 'F':
-        digitalWrite(relay,HIGH);
-        break;
-    }
-    
-  }
-
-  /*----------------------------------------------------------Drain---------------------------------------------*/
-  
+    temp();
+    Serial.println("Percentage: 0");
+    digitalWrite(relay,LOW);
+    delay(3000);
+    Serial.println("On");
+    void bluetoot();
+  } 
   else if (input_voltage < 2.70 && input_voltage >=2.50 )
   {
     byte storageByte = 0xC0;
     updateShiftRegister(storageByte);
     digitalWrite(buzz, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(buzz, LOW);
-    //delay (5000);
+    delay (1000);
     digitalWrite(led, HIGH);
     delay (5000);
     digitalWrite(led, LOW);
-    digitalWrite(led, HIGH);
-    //delay (5000);
+    delay (1000);
     temp();
-    Serial.println("Percentage: 30");
+    Serial.println("Percentage: 20");
     Serial.println("Health:Bad");
     Serial.println("On");
     Serial.println("Drain");
-    while(!bluetooth.available());
-    char value = bluetooth.read();
-    switch(value)
-    {
-      case 'O':
-        digitalWrite(relay,LOW);
-        Serial.println("On");
-        break;
-      case 'F':
-        digitalWrite(relay,HIGH);
-        break;
-    }
     
-  }
-
-  /*--------------------------------------------------------------Drain 1------------------------------------------*/
-   
+    
+  }   
   else if (input_voltage < 3.00 && input_voltage >=2.75 )
   {
     byte storageByte = 0xE0;
     updateShiftRegister(storageByte);
     digitalWrite(buzz, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(buzz, LOW);
-    //delay (5000);
+    delay (1000);
     digitalWrite(led, HIGH);
     delay (5000);
     digitalWrite(led, LOW);
-    digitalWrite(led, HIGH);
-    //delay (5000);
+    delay (1000);
+    digitalWrite(relay,LOW);
+    delay(1000);
     Serial.println("Percentage: 40");
+    Serial.println("Health:Bad");
     temp();
     Serial.println("On");
     Serial.println("Drain");
-    while(!bluetooth.available());
-    char value = bluetooth.read();
-    switch(value)
-    {
-      case 'O':
-        digitalWrite(relay,LOW);
-        Serial.println("On");
-        break;
-      case 'F':
-        digitalWrite(relay,HIGH);
-        break;
-    }
+    
   }
-
-  /*----------------------------------------------------------------Drain 2-------------------------------------------*/
-  
   else if (input_voltage < 3.25 && input_voltage >= 3.00)
   {
     byte storageByte = 0xF0;
     updateShiftRegister(storageByte);
     digitalWrite(buzz, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(buzz, LOW);
-    //delay (5000);
+    delay (1000);
     digitalWrite(led, HIGH);
     delay (5000);
     digitalWrite(led, LOW);
-    digitalWrite(led, HIGH);
-    //delay (5000);
+    delay (1000);
+    digitalWrite(relay,LOW);
+    delay(1000);
     Serial.println("Percentage: 60");
     temp();
+    Serial.println("Health:Bad");
     Serial.println("On");
     Serial.println("Drain");
-    while(!bluetooth.available());
-    char value = bluetooth.read();
-    switch(value)
-    {
-      case 'O':
-        digitalWrite(relay,LOW);
-        Serial.println("On");
-        break;
-      case 'F':
-        digitalWrite(relay,HIGH);
-        break;
-    }
+    
   }
-
-  /*------------------------------------------------------------Drain 3------------------------------------------------*/
-  
   else if (input_voltage < 3.50 && input_voltage >= 3.25)
   {
     byte storageByte = 0xF8;
     updateShiftRegister(storageByte);
     digitalWrite(buzz, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(buzz, LOW);
-    //delay (5000);
+    delay (1000);
     digitalWrite(led, HIGH);
     delay (5000);
     digitalWrite(led, LOW);
-    digitalWrite(led, HIGH);
-    //delay (5000);
+    delay (1000);
+    /*digitalWrite(relay,LOW);
+    delay(1000);*/
     Serial.println("Percentage: 70");
+    Serial.println("Health:Bad");
     temp();
     Serial.println("On");
     Serial.println("Drain");
-    while(!bluetooth.available());
-    char value = bluetooth.read();
-    switch(value)
-    {
-      case 'O':
-        digitalWrite(relay,LOW);
-        Serial.println("On");
-        break;
-      case 'F':
-        digitalWrite(relay,HIGH);
-        break;
-    }
+    
   }
-
-  /*-------------------------------------------------------Drain 4-------------------------------------------------------*/
-  
   else if (input_voltage <3.70 && input_voltage >= 3.50)
   {
-    
     byte storageByte = 0xFC;
     updateShiftRegister(storageByte);
     Serial.println("Percentage: 80");
+    Serial.println("Health:Bad");
     temp();
     
   }
@@ -254,48 +192,31 @@ void loop()
     byte storageByte = 0xFE;
     updateShiftRegister(storageByte);
     Serial.println("Percentage: 90");
+    Serial.println("Health:Good");
     temp();
   }
-  /*----------------------------------------------------- Normal----------------------------------------------------------*/
-
-  
   else if (input_voltage <= 5.00 && input_voltage >= 3.85)
   {
-    
     byte storageByte = 0xFF;
     updateShiftRegister(storageByte);
-    for(int k=0;k<10;k++)
+    for(int k=0;k<2;k++)
     {    
       Serial.println("Full");
     }
-    digitalWrite(relay,HIGH);
-    delay(1000);
     digitalWrite(buzz, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(buzz, LOW);
-    digitalWrite(led, HIGH);   
-    delay(5000);                      
-    digitalWrite(led, LOW);   
-    //delay(5000);
-    //digitalWrite(relay,LOW);
-    Serial.println("Percentage: 100");
+    delay (1000);
+    digitalWrite(led, HIGH);
+    delay (5000);
+    digitalWrite(led, LOW);
+    delay (1000);
+    //digitalWrite(relay,HIGH);
+    //delay(3000);
+    Serial.println("Percentage: 95");
     temp();
-    Serial.println("OFF");
     Serial.println("Health:Good");
-    while(!bluetooth.available());
-    char value = bluetooth.read();
-    switch(value)
-    {
-      case 'O':
-        digitalWrite(relay,LOW);
-        break;
-      case 'F':
-        digitalWrite(relay,HIGH);
-        Serial.println("Off");
-        break;
-    }
-    /*Full Charge*/
-   }
+    Serial.println("OFF");
+    void bluetoot(); 
+   } 
 }
-
-/*-----------------------------------------------------------X-X-X-X-X-X---------------------------------------------------*/    
